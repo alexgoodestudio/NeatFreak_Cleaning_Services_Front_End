b36dfbec1df492dcb1455f2f4eb2727d6b794a68
Alex Goode <alexgoodestudio@gmail.com>
2024-01-06 19:34:18 -0500

    nav fix

--- front-end/src/components/Dashboard.js
+++ front-end/src/components/Dashboard.js
@@ -5,46 +5,41 @@ import "./Dashboard.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function Dashboard() {
-
-
   return (
- 
-
-        <div>
-          <div className="fadeout">
-            <div className="innerColumn d-flex justify-content-center align-items-center">
-              <h1 className="display-6">
-                <span className="italics">Clean&nbsp;</span>Space,&nbsp;
-                <span className="underline">Happy Mind</span>
-              </h1>
-            </div>
-          </div>
-          <div className="fadein">
-            <div className="slide-in row gx-0">
-              <div className="col-lg-6 mt-3 blue2">
-                <div className="text-container d-flex justify-content-center align-items-center">
-                  <div>
-                    <h1 className="display-6">
-                      <span className="italics2">Clean&nbsp;</span>Space,&nbsp;
-                      <span className="underline">Happy Mind</span>
-                    </h1>
-                    <h4 className="thin">Let us handle the cleaning</h4>
-                    <div className="mt-4">
-                      <Link to="/request" className="btn2 mt-2">
-                        <span className="bold">Request&nbsp;</span>
-                        <span className="spacing1"> Estimate</span>
-                      </Link>
-                    </div>
-                  </div>
+    <div>
+      <div className="fadeout">
+        <div className="innerColumn d-flex justify-content-center align-items-center">
+          <h1 className="display-6">
+            <span className="italics">Clean&nbsp;</span>Space,&nbsp;
+            <span className="underline">Happy Mind</span>
+          </h1>
+        </div>
+      </div>
+      <div className="fadein">
+        <div className="slide-in row gx-0">
+          <div className="col-lg-6 mt-3 blue2">
+            <div className="text-container d-flex justify-content-center align-items-center">
+              <div>
+                <h1 className="display-6">
+                  <span className="italics2">Clean&nbsp;</span>Space,&nbsp;
+                  <span className="underline">Happy Mind</span>
+                </h1>
+                <h4 className="thin">Let us handle the cleaning</h4>
+                <div className="mt-4">
+                  <Link to="/request" className="btn2 mt-2">
+                    <span className="bold">Request </span>
+                    <span className="spacing1">Estimate</span>
+                  </Link>
                 </div>
               </div>
-              <div className="col-lg-6">
-                <img src={Image6} className="img-fluid bathroom" alt="..." />
-              </div>
             </div>
           </div>
+          <div className="col-lg-6">
+            <img src={Image6} className="img-fluid bathroom" alt="..." />
+          </div>
         </div>
-
+      </div>
+    </div>
   );
 }
 
