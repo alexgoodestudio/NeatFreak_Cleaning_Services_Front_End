420fd0ff99f4fa69d0f04edaba7a3cfabc9b5612
Alex Goode <alexgoodestudio@gmail.com>
2024-01-08 17:03:42 -0500

    playsinline

--- front-end/src/components/Video.js
+++ front-end/src/components/Video.js
@@ -4,7 +4,7 @@ import myVideo from './Images/trim.mp4';
 function Video() {
   return (
     <div>
-      <video className='img-fluid vid'autoPlay muted loop>
+      <video className='img-fluid vid' autoPlay muted loop preload="metadata" playsinline>
         <source src={myVideo} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
