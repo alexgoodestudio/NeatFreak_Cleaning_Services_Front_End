0258ed680a1c995e0ffb1720ab0104540099d47d
Alex Goode <alexgoodestudio@gmail.com>
2024-01-08 17:23:12 -0500

    fallback mp4 video

--- front-end/src/components/Video.js
+++ front-end/src/components/Video.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import myVideo from './Images/trim.webm'; 
+import myMP4Video from './Images/trim.mp4'; 
 
 function Video() {
   return (
     <div>
       <video className='img-fluid vid' autoPlay muted loop preload="metadata" playsinline>
         <source src={myVideo} type="video/webm" />
+        <source src={myMP4Video} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
     </div>
