a108bb263a4afce078a8e5985d273b12f917c550
Alex Goode <alexgoodestudio@gmail.com>
2024-01-08 19:51:38 -0500

    calendly update

--- front-end/src/components/Calendly.js
+++ front-end/src/components/Calendly.js
@@ -2,13 +2,14 @@ import Iframe from 'react-iframe';
 
 function CalendlyEmbed() {
   return (
-    <Iframe url="https://calendly.com/getneatfreakclean"
+    <Iframe url="https://calendly.com/getneatfreakclean/15min"
             width="100%"
             height="600px"
             id="myId"
             className="myClassname"
             display="initial"
-            position="relative"/>
+            position="relative"
+            allow="fullscreen"/>
   );
 }
 
