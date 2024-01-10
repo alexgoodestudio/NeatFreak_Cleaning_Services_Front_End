284fcc9b213dbc4d1f505388275dae3ec5aa9e2e
Alex Goode <alexgoodestudio@gmail.com>
2024-01-09 19:25:34 -0500

    routes

--- front-end/src/components/Offer.js
+++ front-end/src/components/Offer.js
@@ -3,9 +3,7 @@ import "./Dashboard.css";
 import Image1 from "./Images/markus-spiske-6MB_jBwteIg-unsplash.jpg";
 import Image3 from "./Images/point3d-commercial-imaging-ltd-dsh1MmzaMcw-unsplash.jpg";
 import Image4 from "./Images/Screenshot 2023-12-31 at 12.19.49 PM.png";
-import Footer from "./Footer"
-
-
+import Footer from "./Footer";
 
 function Offer() {
   return (
@@ -14,28 +12,26 @@ function Offer() {
         <div className="col-lg-6">
           <div className="row">
             <h1 className="display-6 ">
-            <span className="italics">Our&nbsp;</span>
-            <span className="slide-in">Services</span>
-          </h1>
-      <hr></hr>
-              
-              <p className=" body subscribePara  blue mt-1 mb-3">
-                At <span className="bold">NeatFreak,</span> we specialize in providing
-                top-tier house cleaning that caters to the unique needs of each
-                client, transforming your space into a pristine, welcoming
-                environment. We pride ourselves on our commitment to
-                eco-friendly cleaning practices, utilizing environmentally safe
-                and sustainable products to ensure not only the cleanliness but
-                also the health and safety of your home. Our services are
-                designed to accommodate various schedules and preferences,
-                offering flexible options like one-time cleanings, as well as
-                weekly, bi-weekly, or monthly appointments. At NeatFreak
-                Cleaning Services, our goal is to deliver exceptional cleaning
-                services that leave your space spotless and provide you with the
-                peace of mind and time to focus on what matters most in your
-                life. 
-              </p>
-           
+              <span className="italics">Our&nbsp;</span>
+              <span className="slide-in">Services</span>
+            </h1>
+            <hr></hr>
+
+            <p className=" body subscribePara  blue mt-1 mb-3">
+              At <span className="bold">NeatFreak,</span> we specialize in
+              providing top-tier house cleaning that caters to the unique needs
+              of each client, transforming your space into a pristine, welcoming
+              environment. We pride ourselves on our commitment to eco-friendly
+              cleaning practices, utilizing environmentally safe and sustainable
+              products to ensure not only the cleanliness but also the health
+              and safety of your home. Our services are designed to accommodate
+              various schedules and preferences, offering flexible options like
+              one-time cleanings, as well as weekly, bi-weekly, or monthly
+              appointments. At NeatFreak Cleaning Services, our goal is to
+              deliver exceptional cleaning services that leave your space
+              spotless and provide you with the peace of mind and time to focus
+              on what matters most in your life.
+            </p>
           </div>
           {/* ------------------------------------------------------------------ */}
           <div className="row  ">
@@ -43,14 +39,17 @@ function Offer() {
               <div className="card  w-100 d-flex">
                 <img src={Image1} className="card-img-top" alt="..." />
                 <div className="card-body cardBody1 ">
-                  <h5 className="card-title underline mb-3">Eco Friendly Products</h5>
+                  <h5 className="card-title underline mb-3">
+                    Eco Friendly Products
+                  </h5>
                   <p className="card-text">
-                  "Green Clean: Safe, sustainable, spotless. Your home, our planet-friendly touch."
-                  
-            </p>
-                  <a href="/products" className="btn btn-secondary btn-custom ">
-                    Eco Friendly
-                  </a>
+                    "Green Clean: Safe, sustainable, spotless. Your home, our
+                    planet-friendly touch."
+                  </p>
+                  <Link to="/products" className="btn btn-secondary">
+                  Eco Friendly
+                  </Link>
+      
                 </div>
               </div>
             </div>
@@ -59,12 +58,16 @@ function Offer() {
               <div className="card  w-100 ">
                 <img src={Image3} className="card-img-top " alt="..." />
                 <div className="card-body ">
-                  <h5 className="card-title underline mb-3">Exceptional Quality</h5>
-                  <p className="card-text">
-                  "Each clean, done with careful attention, leaves your space looking pristine and beautiful."                  </p>
-                  <a href="/reviews" className="btn btn-secondary  ">
+                  <h5 className="card-title underline mb-3">
                     Exceptional Quality
-                  </a>
+                  </h5>
+                  <p className="card-text">
+                    "Each clean, done with careful attention, leaves your space
+                    looking pristine and beautiful."{" "}
+                  </p>
+                  <Link to="/reviews" className="btn btn-secondary">
+                  Exceptional Quality
+                  </Link>
                 </div>
               </div>
             </div>
@@ -73,13 +76,12 @@ function Offer() {
         <div className="col-lg-6 mt-4 ">
           <img src={Image4} className="img-fluid down br" alt="..." />
         </div>
-        
       </div>
-      
+
       <div className="row mt-5">
         <div className="col-12">
-          <Footer/>
-      </div>
+          <Footer />
+        </div>
       </div>
     </div>
   );
