2dfac05980eda2244a961a28d789c5424e5db367
Alex Goode <alexgoodestudio@gmail.com>
2024-01-09 19:46:47 -0500

    playsInline

--- front-end/src/components/Video.js
+++ front-end/src/components/Video.js
@@ -5,14 +5,12 @@ function Video() {
   return (
     <div>
       
-      <video className='w-100 image-container' autoPlay muted loop preload="metadata" playsinline>
+      <video className='w-100 image-container' autoPlay muted loop preload="metadata" playsInline>
         
         <source src={myMP4Video} type="video/mp4" />
         Your browser does not support the video tag.
       </video>
-      {/* <h2 className="overlay-text mobileExpand fontLight    reduce">
-             Join Our Exclusive <span className="italics space ">&nbsp;Cleaning&nbsp;</span> Tips<span className="bold  ">&nbsp;Newsletter!</span>
-          </h2>   */}
+
     </div>
   ); 
 }
