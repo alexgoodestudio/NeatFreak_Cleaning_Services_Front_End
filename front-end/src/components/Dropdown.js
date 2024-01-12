1e3a88f41d88e48846cab088e26d4be8d356352c
Alex Goode <alexgoodestudio@gmail.com>
2024-01-11 23:22:05 -0500

    fix dropdown link

--- front-end/src/components/Dropdown.js
+++ front-end/src/components/Dropdown.js
@@ -1,30 +1,29 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import "./Dashboard.css"
+import "./Dashboard.css";
 
 const Dropdown = () => {
     const [isOpen, setIsOpen] = useState(false);
 
-    const toggleDropdown = () => setIsOpen(!isOpen);
+    const toggleDropdown = (event) => {
+        event.preventDefault(); // Prevents the default link behavior
+        setIsOpen(!isOpen);
+    };
 
     return (
         <div className="dropdown">
             <Link 
-                className=" dropdown-toggle dropdown" 
-                to="./ourservices"
-                type="button" 
-                id="dropdownMenuButton" 
-                data-bs-toggle="dropdown" 
-                aria-expanded={isOpen} 
+                className="dropdown-toggle link marginNav link-hover " 
+                to="#"
                 onClick={toggleDropdown}
             >
                 Our Services
             </Link>
 
             <div className={`dropdown-menu${isOpen ? ' show' : ''}`} aria-labelledby="dropdownMenuButton">
-                <a className="dropdown-item" href="onetime">One-Time Clean</a>
-                <a className="dropdown-item" href="moving">Move In/Out Clean</a>
-                <a className="dropdown-item" href="recurring">Recurring Service</a>
+                <Link className="dropdown-item" to="/onetime">One-Time Clean</Link>
+                <Link className="dropdown-item" to="/moving">Move In/Out Clean</Link>
+                <Link className="dropdown-item" to="/recurring">Recurring Service</Link>
             </div>
         </div>
     );
