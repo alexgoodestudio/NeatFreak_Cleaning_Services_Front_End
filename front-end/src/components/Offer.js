8159b6d7f50f9f852c693df1963286b5d7235c4e
Alex Goode <alexgoodestudio@gmail.com>
2024-03-05 20:36:00 -0500

    implementing design layout principles

--- front-end/src/components/Offer.js
+++ front-end/src/components/Offer.js
@@ -3,11 +3,10 @@ import "./Dashboard.css";
 import Image3 from "./Images/card3.png";
 import Image4 from "./Images/card5.png";
 import Footer from "./Footer";
-import Image5 from "./Images/qwe.png"
+import Image5 from "./Images/qwe.png";
 import { Link } from "react-router-dom";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import Chatbot from "./Chatbot"
-
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Chatbot from "./Chatbot";
 
 function Offer() {
   return (
@@ -15,58 +14,61 @@ function Offer() {
       <div className="container fade-in  mt-5 ">
         <div className="row ">
           <h1 className="display-6">
-            
-            <FontAwesomeIcon icon={['far', 'face-smile']} className="ms-3 me-4 increase"/>
+            {/* <FontAwesomeIcon
+              icon={["far", "face-smile"]}
+              className="ms-3 me-4 increase"
+            /> */}
             <span className="italics">Clean&nbsp;</span>
             <span className="">Space, Happy Mind</span>
-
           </h1>
           <hr></hr>
 
-          <div className="col-lg-9">
-            <p className=" body para pe-2  blue ">
-              At <span className="bold">NeatFreak,</span> we specialize in
-              providing top-tier house cleaning that caters to the unique needs
-              of each client, transforming your space into a pristine, welcoming
-              environment. We pride ourselves on our commitment to eco-friendly
-              cleaning practices, utilizing environmentally safe and sustainable
-              products to ensure not only the cleanliness but also the health
-              and safety of your home.
-            </p>
-            
-            <p className=" body para pe-2  blue ">
-            Our services are designed to accommodate
-              various schedules and preferences, offering flexible options like
-              one-time cleanings, as well as weekly, bi-weekly, or monthly
-              appointments. At NeatFreak Cleaning Services, our goal is to
-              deliver exceptional cleaning services that leave your space
-              spotless and provide you with the peace of mind and time to focus
-              on what matters most in your life.
-            </p>
+          <div className="col-lg-8">
+            <div className="col-lg-9">
+              {" "}
+              <p className=" body para pe-2  blue ">
+                At <span className="bold">NeatFreak,</span> we specialize in
+                providing top-tier house cleaning that caters to the unique
+                needs of each client, transforming your space into a pristine,
+                welcoming environment. We pride ourselves on our commitment to
+                eco-friendly cleaning practices, utilizing environmentally safe
+                and sustainable products to ensure not only the cleanliness but
+                also the health and safety of your home.
+              </p>
+              <p className=" body para pe-2  blue ">
+                Our services are designed to accommodate various schedules and
+                preferences, offering flexible options like one-time cleanings,
+                as well as weekly, bi-weekly, or monthly appointments. At
+                NeatFreak Cleaning Services, our goal is to deliver exceptional
+                cleaning services that leave your space spotless and provide you
+                with the peace of mind and time to focus on what matters most in
+                your life.
+              </p>
+            </div>
+            <div className="col-lg-4"></div>
           </div>
-          <div className="col-lg-3 mt-lg-3 mt-1 mb-lg-4 mb-1">
-            
-          <div className="card">
-  <div className="card-header">Our Services</div>
-  <ul className="list-group list-group-flush">
-    <li className="list-group-item">
-      <Link to="/onetime" className="service-link">
-        One Time Clean
-      </Link>
-    </li>
-    <li className="list-group-item">
-      <Link to="/moving" className="service-link">
-        Moving In/ Moving Out
-      </Link>
-    </li>
-    <li className="list-group-item">
-      <Link to="/recurring" className="service-link">
-        Recurring Service
-      </Link>
-    </li>
-  </ul>
-</div>
-
+          <div className="col-lg-1"></div>
+          <div className="col-lg-3 p-3  mt-lg-3 mt-1 mb-lg-4 mb-1">
+            <div className="card">
+              <div className="card-header">Our Services</div>
+              <ul className="list-group list-group-flush">
+                <li className="list-group-item ">
+                  <Link to="/onetime" className="service-link">
+                    One Time Clean
+                  </Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/moving" className="service-link">
+                    Moving In/ Moving Out
+                  </Link>
+                </li>
+                <li className="list-group-item">
+                  <Link to="/recurring" className="service-link">
+                    Recurring Service
+                  </Link>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
@@ -84,7 +86,10 @@ function Offer() {
                       A thorough, one-off cleaning service using eco-friendly
                       products for a sparkling, healthy home.
                     </p>
-                    <Link to="/onetime" className="  btn btn-outline-secondary ">
+                    <Link
+                      to="/onetime"
+                      className="  btn btn-outline-secondary "
+                    >
                       One-Time Clean
                     </Link>
                   </div>
@@ -116,26 +121,26 @@ function Offer() {
                       Regular cleaning schedules tailored to your needs,
                       maintaining cleanliness with eco-safe products.
                     </p>
-                    <Link to="/recurring" className=" btn btn-outline-secondary ">
+                    <Link
+                      to="/recurring"
+                      className=" btn btn-outline-secondary "
+                    >
                       Recurring Service
                     </Link>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="col-lg-3 ">
-            
-            </div>
+            <div className="col-lg-3 "></div>
           </div>
         </div>
       </div>
 
       {/* ------------------------------------------------------------------ */}
-<Chatbot/>
+      <Chatbot />
       <div className="scaleDown">
         <Footer />
       </div>
-      
     </div>
   );
 }
